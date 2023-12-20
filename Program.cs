a6e1d32f99c033d7a7652945d33ef95d60e630c5
Victor Nascimento Aguiar <112593695+victorNascimentoAguiar@users.noreply.github.com>
2023-12-20 18:29:33 -0300

    um codigo usando propriedades metodos e construtores

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Propriedades.Models;
+
+Pessoa p1 = new Pessoa (nome:"victor", sobrenome: "Nascimento");
+
+
+Pessoa p2 = new Pessoa(nome:"Junior", sobrenome:"Freitas" );
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
